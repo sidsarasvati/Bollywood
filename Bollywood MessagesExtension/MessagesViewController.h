3fd3ad1fe3a5985e78a34f9496e7f9e782d67906
Sid Sarasvati <sid@cogitocorp.com>
2017-12-02 21:20:54 -0500

    Initial Commit

---
+++ Bollywood MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  Bollywood MessagesExtension
+//
+//  Created by Sid Sarasvati on 12/2/17.
+//  Copyright © 2017 madFrog. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
